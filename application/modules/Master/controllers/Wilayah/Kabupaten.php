f0e17e57dd21518f562e2c9432f68220aef0f491
maspriyambodo <maspriyambodo>
2021-05-18 09:57:50 +0700

    add modules master->wilayah:

--- application/modules/Master/controllers/Wilayah/Kabupaten.php
+++ application/modules/Master/controllers/Wilayah/Kabupaten.php
@@ -6,11 +6,13 @@ class Kabupaten extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+        $this->load->model('M_kabupaten', 'model');
         $this->user = $this->bodo->Dec($this->session->userdata('id_user'));
     }
 
     public function index() {
         $data = [
+            'prov' => $this->model->Provinsi(),
             'csrf' => $this->bodo->Csrf(),
             'item_active' => 'Master/Wilayah/Kabupaten/index/',
             'privilege' => $this->bodo->Check_previlege('Master/Wilayah/Kabupaten/index/'),
@@ -24,8 +26,101 @@ class Kabupaten extends CI_Controller {
                 ]
             ]
         ];
-        $data['content'] = $this->parser->parse('wilayah/kabupaten', $data, true);
+        $data['content'] = $this->parser->parse('wilayah/kabupaten/index', $data, true);
         return $this->parser->parse('Template/layout', $data);
+    }
+
+    public function Lists() {
+        $list = $this->model->lists();
+        $data = [];
+        $no = Post_input("start");
+        $privilege = $this->bodo->Check_previlege('Master/Wilayah/Kabupaten/index/');
+        foreach ($list as $kabupaten) {
+            $id_kabupaten = Enkrip($kabupaten->id_kabupaten);
+            if ($kabupaten->is_actived) {
+                $stat = '<span class="label label-success label-inline font-weight-lighter mr-2">active</span>';
+            } else {
+                $stat = '<span class="label label-danger label-inline font-weight-lighter mr-2">nonactive</span>';
+            }
+            if ($privilege['update']) {
+                $editbtn = '<button id="edit_user" type="button" class="btn btn-icon btn-default btn-xs" title="Edit ' . $kabupaten->nama . '" value="' . $id_kabupaten . '" onclick="Edit(this.value)"><i class="far fa-edit"></i></button>';
+            } else {
+                $editbtn = null;
+            }
+            if ($privilege['delete'] and $kabupaten->is_actived) {
+                $activebtn = null;
+                $delbtn = '<button id="del_user" type="button" class="btn btn-icon btn-danger btn-xs" title="Delete ' . $kabupaten->nama . '" value="' . $id_kabupaten . '" onclick="Delete(this.value)"><i class="far fa-trash-alt"></i></button>';
+            } elseif ($privilege['delete'] and!$kabupaten->is_actived) {
+                $delbtn = null;
+                $activebtn = '<button id="act_user" type="button" class="btn btn-icon btn-success btn-xs" title="Activate ' . $kabupaten->nama . '" value="' . $id_kabupaten . '" onclick="Active(this.value)"><i class="fas fa-unlock"></i></button>';
+            } else {
+                $delbtn = null;
+                $activebtn = null;
+            }
+            $no++;
+            $row = [];
+            $row[] = $no;
+            $row[] = $kabupaten->id_kabupaten;
+            $row[] = $kabupaten->nama;
+            $row[] = $stat;
+            $row[] = $kabupaten->longitude;
+            $row[] = $kabupaten->latitude;
+            $row[] = '<a href="https://www.google.com/maps/place/' . $kabupaten->nama . '" class="btn btn-icon btn-default btn-xs" title="View ' . $kabupaten->nama . ' on maps" target="_blank"><i class="fas fa-map-marked-alt"></i></a>';
+            $row[] = '<div class="btn-group">' . $editbtn . $delbtn . $activebtn . '</div>';
+            $data[] = $row;
+        }
+        return $this->_list($data, $privilege);
+    }
+
+    private function _list($data, $privilege) {
+        if ($privilege['read']) {
+            $output = [
+                "draw" => Post_input('draw'),
+                "recordsTotal" => $this->model->count_all(),
+                "recordsFiltered" => $this->model->count_filtered(),
+                "data" => $data
+            ];
+        } else {
+            $output = [
+                "draw" => Post_input('draw'),
+                "recordsTotal" => 0,
+                "recordsFiltered" => 0,
+                "data" => []
+            ];
+        }
+        ToJson($output);
+    }
+
+    public function Get_id() {
+        $id = Post_get('id_kab');
+        $exec = $this->model->Get_id($id);
+        if ($exec) {
+            $response = [
+                'stat' => false,
+                'msg' => 'ID Kabupaten already exist!'
+            ];
+        } else {
+            $response = [
+                'stat' => true,
+                'msg' => 'ID Kabupaten available to use!'
+            ];
+        }
+        ToJson($response);
+    }
+
+    public function Add() {
+        $id_provinsi = $this->bodo->Dec(Post_input('add_prov'));
+        $data = [
+            'id_kabupaten' => Post_input('add_idkab'),
+            'id_provinsi' => $id_provinsi,
+            'nama' => Post_input('add_namakab'),
+            'is_actived' => 1,
+            'latitude' => Post_input('add_lat'),
+            'longitude' => Post_input('add_longt'),
+            'syscreateuser' => $this->user,
+            'syscreatedate' => date("Y-m-d H:i:s")
+        ];
+        $this->model->Add($data);
     }
 
 }
--- application/modules/Master/models/M_kabupaten.php
+++ application/modules/Master/models/M_kabupaten.php
@@ -2,23 +2,15 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class M_country extends CI_Model {
+class M_kabupaten extends CI_Model {
 
-    var $table = 'mt_country';
-    var $column_order = array(null, 'code', 'country'); //set column field database for datatable orderable
-    var $column_search = array('code', 'country'); //set column field database for datatable searchable 
-    var $order = array('id' => 'asc'); // default order 
-
-    public function index($data) {
-        $exec = $this->db->query('CALL mt_country("' . $data['param'] . '",' . $data['country_id'] . ',"' . $data['kode_negara'] . '","' . $data['nama_negara'] . '","' . $data['bendera'] . '",' . $data['user_login'] . ');');
-        mysqli_next_result($this->db->conn_id);
-        return $exec;
-    }
+    var $table = 'mt_wil_kabupaten';
+    var $column_order = [null, 'id_kabupaten', 'nama', 'is_actived', 'longitude', 'latitude', null]; //set column field database for datatable orderable
+    var $column_search = ['id_kabupaten', 'nama', 'longitude', 'latitude']; //set column field database for datatable searchable 
+    var $order = array('id_kabupaten' => 'asc'); // default order
 
     private function _get_datatables_query() {
-        $this->db->select('mt_country.id AS id_country, mt_country.`code` AS code_country, mt_country.country AS nama_country, mt_country.stat, mt_country.flags')
-                ->from($this->table)
-                ->where('mt_country.stat', 1, false);
+        $this->db->from($this->table);
         $i = 0;
         foreach ($this->column_search as $item) { // loop column 
             if ($_POST['search']['value']) { // if datatable send POST for search
@@ -60,6 +52,35 @@ class M_country extends CI_Model {
     public function count_all() {
         $this->db->from($this->table);
         return $this->db->count_all_results();
+    }
+
+    public function Get_id($id) {
+        $exec = $this->db->select('mt_wil_kabupaten.id_kabupaten')
+                ->from('mt_wil_kabupaten')
+                ->where('mt_wil_kabupaten.id_kabupaten', $id, false)
+                ->get()
+                ->row();
+        return $exec;
+    }
+
+    public function Provinsi() {
+        $exec = $this->db->select('mt_wil_provinsi.id_provinsi, mt_wil_provinsi.nama')
+                ->from('mt_wil_provinsi')
+                ->get()
+                ->result();
+        return $exec;
+    }
+
+    public function Add($data) {
+        $this->db->trans_begin();
+        $this->db->insert('mt_wil_kabupaten', $data);
+        if ($this->db->trans_status() === false) {
+            $this->db->trans_rollback();
+            redirect(base_url(), $this->session->set_flashdata('err_msg', 'error while inserting new kabupaten'));
+        } else {
+            $this->db->trans_commit();
+            redirect(base_url(), $this->session->set_flashdata('succ_msg', 'new kabupaten has been added'));
+        }
     }
 
 }
