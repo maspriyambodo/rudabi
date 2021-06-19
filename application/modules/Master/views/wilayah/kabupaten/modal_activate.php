52d9c787724aee33b2422ce82d5c020b174e2fad
maspriyambodo <maspriyambodo>
2021-06-19 22:43:50 +0700

    resolve error 	modified:   application/modules/Master/views/wilayah/kabupaten/modal_activate.php

--- application/modules/Master/views/wilayah/kabupaten/modal_activate.php
+++ application/modules/Master/views/wilayah/kabupaten/modal_activate.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modal_activeLabel">modal title</h5>
+                <h5 class="modal-title" id="modal_activeLabel">Activate Kabupaten</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <i aria-hidden="true" class="fas fa-times"></i>
                 </button>
@@ -15,7 +15,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-light-primary font-weight-bold" data-dismiss="modal"><i class="far fa-times-circle"></i> Cancel</button>
-                    <button type="submit" class="btn btn-danger font-weight-bold"><i class="far fa-trash-alt"></i> Delete</button>
+                    <button type="submit" class="btn btn-success font-weight-bold"><i class="fas fa-unlock"></i> Activate</button>
                 </div>
             </form>
         </div>
