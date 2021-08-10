3636fd436a24aef7471e5375e5492bea1479074c
maspriyambodo <maspriyambodo>
2021-08-10 09:30:02 +0700

    add module SIMKAH: 	new file:   application/modules/Applications/controllers/Simkah/Simkah.php 	new file:   application/modules/Applications/views/simkah/v_index.php

---
+++ application/modules/Applications/controllers/Simkah/Simkah.php
@@ -0,0 +1,73 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * Product:        System of kementerian agama Republik Indonesia
+ * License Type:   Government
+ * Access Type:    Multi-User
+ * License:        https://maspriyambodo.com
+ * maspriyambodo@gmail.com
+ * 
+ * Thank you,
+ * maspriyambodo
+ */
+
+/**
+ * Description of Simkah
+ *
+ * @author centos
+ */
+class Simkah extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->user = $this->bodo->Dec($this->session->userdata('id_user'));
+    }
+
+    public function index() {
+        $year = Post_get('year');
+        $data = [
+            'year' => $year,
+            'item_active' => 'Applications/Simkah/index/?year=0',
+            'privilege' => $this->bodo->Check_previlege('Applications/Simkah/index/?year=0'),
+            'siteTitle' => 'Data SIMKAH | ' . $this->bodo->Sys('app_name'),
+            'pagetitle' => 'Data SIMKAH Tahun ' . ($year == 0 ? date('Y') : $year),
+            'breadcrumb' => [
+                0 => [
+                    'nama' => 'simkah',
+                    'link' => null,
+                    'status' => true
+                ]
+            ]
+        ];
+        $data['content'] = $this->parser->parse('simkah/v_index', $data, true);
+        return $this->parser->parse('Template/layout', $data);
+    }
+
+    public function Get_nikah() {
+        $year = Post_get('year');
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://simkah.kemenag.go.id/api/grafik/daftarnikah?tahun=' . ($year == 0 ? date('Y') : $year) . '&level=pusat',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Basic c2lta2FoX3NpbTpzaW1rYWhzaW1AIQ==',
+                'Cookie: cookiesession1=678B28A6WYZABCDEFHIJKLMNOPQR08DC',
+                'Accept: */*',
+                'Connection: keep-alive',
+                'Accept-Encoding: gzip, deflate, br',
+                'Content-Type: application/json'
+            )
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        echo $response;
+    }
+
+}
---
+++ application/modules/Applications/views/simkah/v_index.php
@@ -0,0 +1,89 @@
+<div class="card card-custom" data-card="true" id="kt_card_1">
+    <div class="card-header">
+        <div class="card-title">
+            Data Nikah per Provinsi
+        </div>
+        <div class="card-toolbar">
+            <a href="#" class="btn btn-icon btn-sm btn-hover-light-primary mr-1" data-card-tool="toggle" data-toggle="tooltip" data-placement="top" title="Minimalkan">
+                <i class="ki ki-arrow-down icon-nm"></i>
+            </a>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="text-center">
+            <b><u id="title_chartdiv"></u></b>
+        </div>
+        <div id="chartdiv" class="chartdivs"></div>
+    </div>
+</div>
+<script>
+    $(document).ready(function () {
+        var dt_nikah = function () {
+            var tmp = null;
+            $.ajax({
+                url: "<?php echo base_url('Applications/Simkah/Get_nikah?year=' . $year); ?>",
+                async: false,
+                type: 'GET',
+                cache: true,
+                contentType: false,
+                processData: false,
+                dataType: 'json',
+                success: function (result) {
+                    var data1 = JSON.stringify(result.data);
+                    var obj = jQuery.parseJSON(data1);
+                    var i, arr, tot;
+                    tot = 0;
+                    for (i = 0; i < obj.length; i++) {
+                        arr = parseFloat(obj[i].value);
+                        tot += arr;
+                        
+                    }
+                    document.getElementById('title_chartdiv').innerText = 'Total Data Nikah: ' + numeral(tot).format('0,0');
+                    tmp = obj;
+                }
+            });
+            return tmp;
+        }();
+        am4core.ready(function () {
+            am4core.useTheme(am4themes_animated);
+            var chart = am4core.create("chartdiv", am4charts.XYChart);
+            chart.scrollbarX = new am4core.Scrollbar();
+            chart.data = dt_nikah;
+            chart.exporting.menu = new am4core.ExportMenu();
+            var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+            categoryAxis.title.fontWeight = 800;
+            categoryAxis.title.text = 'Daerah Tingkat Provinsi';
+            categoryAxis.dataFields.category = "name";
+            categoryAxis.renderer.grid.template.location = 0;
+            categoryAxis.renderer.minGridDistance = 30;
+            categoryAxis.renderer.labels.template.horizontalCenter = "right";
+            categoryAxis.renderer.labels.template.verticalCenter = "middle";
+            categoryAxis.renderer.labels.template.rotation = 270;
+            categoryAxis.tooltip.disabled = true;
+            categoryAxis.renderer.minHeight = 110;
+            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+            valueAxis.renderer.minWidth = 50;
+            valueAxis.title.text = "Jumlah Data Nikah";
+            valueAxis.title.fontWeight = 800;
+            var series = chart.series.push(new am4charts.ColumnSeries());
+            series.sequencedInterpolation = true;
+            series.dataFields.valueY = "value";
+            series.dataFields.categoryX = "name";
+            series.tooltipText = "Jumlah Data Nikah {name}: [bold]{valueY}[/]";
+            series.columns.template.strokeWidth = 0;
+            series.tooltip.pointerOrientation = "vertical";
+            series.columns.template.column.cornerRadiusTopLeft = 10;
+            series.columns.template.column.cornerRadiusTopRight = 10;
+            series.columns.template.column.fillOpacity = 0.8;
+            var hoverState = series.columns.template.column.states.create("hover");
+            hoverState.properties.cornerRadiusTopLeft = 0;
+            hoverState.properties.cornerRadiusTopRight = 0;
+            hoverState.properties.fillOpacity = 1;
+            series.columns.template.adapter.add("fill", function (fill, target) {
+                return chart.colors.getIndex(target.dataItem.index);
+            });
+            chart.cursor = new am4charts.XYCursor();
+        });
+    });
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js" integrity="sha512-USPCA7jmJHlCNRSFwUFq3lAm9SaOjwG8TaB8riqx3i/dAJqhaYilVnaf2eVUH5zjq89BU6YguUuAno+jpRvUqA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
