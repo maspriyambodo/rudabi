846b3381110601b48ec7f26b864269b9671b3be5
maspriyambodo <maspriyambodo>
2021-08-15 19:26:54 +0700

    remove number_format: 	modified:   application/controllers/Dashboard_cron.php

--- application/controllers/Dashboard_cron.php
+++ application/controllers/Dashboard_cron.php
@@ -62,9 +62,9 @@ class Dashboard_cron extends CI_Controller {
     private function sihat() {
         $sihat = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/siihat/total?KEY=BOBA');
         $data = [
-            'alat_hisab_rukyat' => number_format($sihat[0]->alat_hisab_rukyat, 0),
-            'tenaga_ahli' => number_format($sihat[1]->tenaga_ahli, 0),
-            'hisab_pengukuran' => number_format($sihat[2]->hisab_pengukuran, 0)
+            'alat_hisab_rukyat' => $sihat[0]->alat_hisab_rukyat,
+            'tenaga_ahli' => $sihat[1]->tenaga_ahli,
+            'hisab_pengukuran' => $sihat[2]->hisab_pengukuran
         ];
         return $data;
     }
@@ -72,7 +72,7 @@ class Dashboard_cron extends CI_Controller {
     private function masjid() {
         $masjid = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/eimas/dtmasjid?KEY=BOBA');
         $data = [
-            'data_masjid' => number_format($masjid[0]->data_masjid, 0)
+            'data_masjid' => $masjid[0]->data_masjid
         ];
         return $data;
     }
@@ -80,7 +80,7 @@ class Dashboard_cron extends CI_Controller {
     private function mushalla() {
         $mushalla = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/eimas/dtmushalla?KEY=BOBA');
         $data = [
-            'data_mushalla' => number_format($mushalla[0]->data_mushalla, 0)
+            'data_mushalla' => $mushalla[0]->data_mushalla
         ];
         return $data;
     }
@@ -88,7 +88,7 @@ class Dashboard_cron extends CI_Controller {
     private function targetcatin() {
         $targetcatin = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/embimwin/total_targetcatin?KEY=BOBA');
         $data = [
-            'realisasi_wilayah' => number_format($targetcatin[0]->realisasi_wilayah, 0)
+            'realisasi_wilayah' => $targetcatin[0]->realisasi_wilayah
         ];
         return $data;
     }
@@ -96,7 +96,7 @@ class Dashboard_cron extends CI_Controller {
     private function data_catin() {
         $data_catin = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/embimwin/total_datacatin?KEY=BOBA');
         $data = [
-            'jumlah_peserta' => number_format($data_catin[0]->jumlah_peserta, 0)
+            'jumlah_peserta' => $data_catin[0]->jumlah_peserta
         ];
         return $data;
     }
@@ -104,7 +104,7 @@ class Dashboard_cron extends CI_Controller {
     private function pustakadigital() {
         $pustakadigital = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/esbsnn/pustakadigital?KEY=BOBA');
         $data = [
-            'pustakadigital' => number_format($pustakadigital[0]->total, 0)
+            'pustakadigital' => $pustakadigital[0]->total
         ];
         return $data;
     }
@@ -112,9 +112,9 @@ class Dashboard_cron extends CI_Controller {
     private function simpenghulu() {
         $simpenghulu = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/simpenghulu/total?KEY=BOBA');
         $data = [
-            'data_kua' => number_format($simpenghulu[0]->data_kua, 0),
-            'data_penghulu' => number_format($simpenghulu[1]->data_penghulu, 0),
-            'data_peristiwa_nikah' => number_format($simpenghulu[2]->data_peristiwa_nikah, 0)
+            'data_kua' => $simpenghulu[0]->data_kua,
+            'data_penghulu' => $simpenghulu[1]->data_penghulu,
+            'data_peristiwa_nikah' => $simpenghulu[2]->data_peristiwa_nikah
         ];
         return $data;
     }
@@ -122,7 +122,7 @@ class Dashboard_cron extends CI_Controller {
     private function lptq() {
         $lptq = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/simpenaiss/lptq?KEY=BOBA');
         $data = [
-            'lptq' => number_format($lptq[0]->lptq, 0)
+            'lptq' => $lptq[0]->lptq
         ];
         return $data;
     }
@@ -130,7 +130,7 @@ class Dashboard_cron extends CI_Controller {
     private function ormasislam() {
         $ormas_islam = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/simpenaiss/ormasislam?KEY=BOBA');
         $data = [
-            'ormas_islam' => number_format($ormas_islam[0]->ormas_islam, 0)
+            'ormas_islam' => $ormas_islam[0]->ormas_islam
         ];
         return $data;
     }
@@ -138,7 +138,7 @@ class Dashboard_cron extends CI_Controller {
     private function penyuluh() {
         $penyuluh = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/epay/totalnew?KEY=BOBA');
         $data = [
-            'penyuluh' => number_format($penyuluh[0]->penyuluh, 0)
+            'penyuluh' => $penyuluh[0]->penyuluh
         ];
         return $data;
     }
@@ -146,7 +146,7 @@ class Dashboard_cron extends CI_Controller {
     private function siwak() {
         $siwak = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/siwaks/wakaf?KEY=BOBA');
         $data = [
-            'tanah_wakaf' => number_format($siwak[0]->tanah_wakaf)
+            'tanah_wakaf' => $siwak[0]->tanah_wakaf
         ];
         return $data;
     }
@@ -154,7 +154,7 @@ class Dashboard_cron extends CI_Controller {
     private function baznas() {
         $baznas = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/simzat/totaldatabaznas?KEY=BOBA');
         $data = [
-            'databaznas' => number_format($baznas[0]->databaznas, 0)
+            'databaznas' => $baznas[0]->databaznas
         ];
         return $data;
     }
@@ -162,7 +162,7 @@ class Dashboard_cron extends CI_Controller {
     private function laznas() {
         $laznas = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/simzat/totaldatalaznas?KEY=BOBA');
         $data = [
-            'datalaznas' => number_format($laznas[0]->datalaznas, 0)
+            'datalaznas' => $laznas[0]->datalaznas
         ];
         return $data;
     }
@@ -170,7 +170,7 @@ class Dashboard_cron extends CI_Controller {
     private function pustakaslim() {
         $jumlah_buku = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/pustaka/totalbuku?KEY=BOBA');
         $data = [
-            'jumlah_buku' => number_format($jumlah_buku[0]->jumlah_buku, 0)
+            'jumlah_buku' => $jumlah_buku[0]->jumlah_buku
         ];
         return $data;
     }
@@ -178,7 +178,7 @@ class Dashboard_cron extends CI_Controller {
     private function mtq() {
         $mtq = $this->curl->get('https://rudabi.kemenag.dev/rudabi_api/datapi/Mtq/totalpeserta?KEY=BOBA');
         $data = [
-            'tot_mtq' => number_format($mtq[0]->total, 0)
+            'tot_mtq' => $mtq[0]->total
         ];
         return $data;
     }
