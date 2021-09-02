9c4bfede39d8b6acb470f6bdeb140c019d812f70
maspriyambodo <maspriyambodo>
2021-09-02 11:35:55 +0700

    Changes to be committed: 	modified:   application/modules/Applications/views/simkah/v_index.php

--- application/modules/Applications/views/simkah/v_index.php
+++ application/modules/Applications/views/simkah/v_index.php
@@ -17,7 +17,7 @@
     </div>
 </div>
 <script>
-    $(document).ready(function () {
+    window.onload = function () {
         var dt_nikah = function () {
             var tmp = null;
             $.ajax({
@@ -84,6 +84,70 @@
                 return chart.colors.getIndex(target.dataItem.index);
             });
             chart.cursor = new am4charts.XYCursor();
+        });
+    };
+    $(document).ready(function () {
+        $.ajax({
+            url: "<?php echo base_url('Applications/Simkah/Update_simkah'); ?>",
+            async: true,
+            type: 'GET',
+            cache: true,
+            contentType: false,
+            processData: false,
+            dataType: 'json',
+            success: function (result) {
+                var i, arr, tot;
+                    tot = 0;
+                    for (i = 0; i < result.length; i++) {
+                        arr = parseFloat(result[i].jumlah);
+                        tot += arr;
+
+                    }
+                    document.getElementById('title_chartdiv').innerText = 'Total Data Nikah: ' + numeral(tot).format('0,0');
+                am4core.ready(function () {
+                    am4core.useTheme(am4themes_animated);
+                    var chart = am4core.create("chartdiv", am4charts.XYChart);
+                    chart.scrollbarX = new am4core.Scrollbar();
+                    chart.data = result;
+                    chart.exporting.menu = new am4core.ExportMenu();
+                    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+                    categoryAxis.title.fontWeight = 800;
+                    categoryAxis.title.text = 'Daerah Tingkat Provinsi';
+                    categoryAxis.dataFields.category = "provinsi";
+                    categoryAxis.renderer.grid.template.location = 0;
+                    categoryAxis.renderer.minGridDistance = 30;
+                    categoryAxis.renderer.labels.template.horizontalCenter = "right";
+                    categoryAxis.renderer.labels.template.verticalCenter = "middle";
+                    categoryAxis.renderer.labels.template.rotation = 300;
+                    categoryAxis.tooltip.disabled = true;
+                    categoryAxis.renderer.minHeight = 110;
+                    let label = categoryAxis.renderer.labels.template;
+                    label.wrap = true;
+                    label.maxWidth = 120;
+                    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+                    valueAxis.renderer.minWidth = 50;
+                    valueAxis.title.text = "Jumlah Data Nikah";
+                    valueAxis.title.fontWeight = 800;
+                    var series = chart.series.push(new am4charts.ColumnSeries());
+                    series.sequencedInterpolation = true;
+                    series.dataFields.valueY = "jumlah";
+                    series.dataFields.categoryX = "provinsi";
+                    series.tooltipText = "Jumlah Data Nikah {provinsi}: [bold]{valueY}[/]";
+                    series.columns.template.strokeWidth = 0;
+                    series.tooltip.pointerOrientation = "vertical";
+                    series.columns.template.column.cornerRadiusTopLeft = 10;
+                    series.columns.template.column.cornerRadiusTopRight = 10;
+                    series.columns.template.column.fillOpacity = 0.8;
+                    var hoverState = series.columns.template.column.states.create("hover");
+                    hoverState.properties.cornerRadiusTopLeft = 0;
+                    hoverState.properties.cornerRadiusTopRight = 0;
+                    hoverState.properties.fillOpacity = 1;
+                    series.columns.template.adapter.add("fill", function (fill, target) {
+                        return chart.colors.getIndex(target.dataItem.index);
+                    });
+                    chart.cursor = new am4charts.XYCursor();
+                });
+            }
         });
     });
 </script>
