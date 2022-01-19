3331a51e4c88c23873874fc4ab156adc31a9592e
maspriyambodo <maspriyambodo@gmail.com>
2022-01-19 12:11:42 +0700

    add db simbi: 	modified:   application/config.production/database.php

--- application/config.production/database.php
+++ application/config.production/database.php
@@ -23,3 +23,24 @@ $db['default'] = ['dsn' => '',
     'failover' => [],
     'save_queries' => true
 ];
+$db['simbi'] = [
+    'dsn' => '',
+    'hostname' => '10.1.99.145',
+    'username' => 'root',
+    'password' => 'Masdatin2021!',
+    'database' => 'db_simbi',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => false,
+    'db_debug' => false,
+    'cache_on' => false,
+    'cachedir' => '',
+    'char_set' => 'utf8mb4',
+    'dbcollat' => 'utf8mb4_general_ci',
+    'swap_pre' => '',
+    'encrypt' => false,
+    'compress' => false,
+    'stricton' => false,
+    'failover' => [],
+    'save_queries' => true
+];
