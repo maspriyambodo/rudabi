fbc27d3377aae42c8e926a9a8aef64c2f038233b
maspriyambodo <maspriyambodo@gmail.com>
2022-02-21 12:00:37 +0700

    add module example API: new file:   application/modules/api/controllers/sekretariat/Esbsn.php new file:   application/modules/api/models/m_sekretariat/M_sbsn.php

---
+++ application/modules/api/controllers/sekretariat/Esbsn.php
@@ -0,0 +1,99 @@
+<?php
+
+defined('BASEPATH') OR exit('trying to signin backdoor?');
+
+class Esbsn extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('m_sekretariat/m_sbsn', 'model');
+        $this->user = Dekrip($this->session->userdata('id_user'));
+    }
+
+    private function auth() {
+        //http://rudabi.io/api/sekretariat/esbsn/lists?token=YWRtaW46YQ==
+        $token = explode(':', base64_decode(Post_get('token')));
+        if (!empty($token)) {
+            $data = [
+                'uname' => $token[0],
+                'pwd' => $token[1]
+            ];
+            $exec = $this->model->Signin($data);
+            if (!empty($exec) and ($exec->limit_login == 0 or $exec->limit_login != 3)) {
+                $hashed = $exec->pwd;
+                if (password_verify($data['pwd'], $hashed)) {
+                    $this->bodo->Set_session($exec);
+                    $result = true;
+                } else {
+                    $result = die('your password was incorrect');
+                }
+            }
+        } else {
+            $result = die('you are not recognized');
+        }
+        return $result;
+    }
+
+    public function lists() {
+        $this->auth();
+        $list = $this->model->lists();
+        $data = [];
+        $no = Post_get("start");
+        $privilege = $this->bodo->Check_previlege('Systems/Users/index/');
+        foreach ($list as $users) {
+            $id_user = Enkrip($users->id);
+            if ($users->stat) {
+                $stat = '<span class="label label-success label-inline font-weight-lighter mr-2">active</span>';
+            } else {
+                $stat = '<span class="label label-danger label-inline font-weight-lighter mr-2">nonactive</span>';
+            }
+            if ($privilege['update']) {
+                $editbtn = '<button id="edit_user" type="button" class="btn btn-icon btn-default btn-xs" title="Edit ' . $users->uname . '" value="' . $id_user . '" onclick="Edit(this.value)"><i class="far fa-edit"></i></button>';
+            } else {
+                $editbtn = null;
+            }
+            if ($privilege['delete'] and $users->stat) {
+                $activebtn = null;
+                $delbtn = '<button id="del_user" type="button" class="btn btn-icon btn-danger btn-xs" title="Delete ' . $users->uname . '" value="' . $id_user . '" onclick="Delete(this.value)"><i class="far fa-trash-alt"></i></button>';
+                $reset_pwd = '<button id="reset_password" type="button" class="btn btn-icon btn-default btn-xs" title="Reset Password ' . $users->uname . '" value="' . $id_user . '" onclick="Reset_pwd(this.value)"><i class="fas fa-key"></i></button>';
+            } elseif ($privilege['delete'] and!$users->stat) {
+                $delbtn = null;
+                $reset_pwd = '<button id="reset_password" type="button" class="btn btn-icon btn-default btn-xs" title="Reset Password ' . $users->uname . '" value="' . $id_user . '" onclick="Reset_pwd(this.value)"><i class="fas fa-key"></i></button>';
+                $activebtn = '<button id="act_user" type="button" class="btn btn-icon btn-success btn-xs" title="Activate ' . $users->uname . '" value="' . $id_user . '" onclick="Active(this.value)"><i class="fas fa-unlock"></i></button>';
+            } else {
+                $delbtn = null;
+                $activebtn = null;
+                $reset_pwd = null;
+            }
+            $no++;
+            $row = [];
+            $row[] = $no;
+            $row[] = $users->uname;
+            $row[] = $users->name;
+            $row[] = $stat;
+            $row[] = '<div class="btn-group">' . $editbtn . $reset_pwd . $delbtn . $activebtn . '</div>';
+            $data[] = $row;
+        }
+        return $this->_list($data, $privilege);
+    }
+
+    private function _list($data, $privilege) {
+        if ($privilege['read']) {
+            $output = [
+                "draw" => Post_get('draw'),
+                "recordsTotal" => $this->model->count_all(),
+                "recordsFiltered" => $this->model->count_filtered(),
+                "data" => $data
+            ];
+        } else {
+            $output = [
+                "draw" => Post_get('draw'),
+                "recordsTotal" => 0,
+                "recordsFiltered" => 0,
+                "data" => []
+            ];
+        }
+        return ToJson($output);
+    }
+
+}
---
+++ application/modules/api/models/m_sekretariat/M_sbsn.php
@@ -0,0 +1,86 @@
+<?php
+
+defined('BASEPATH') OR exit('trying to signin backdoor?');
+
+class M_sbsn extends CI_Model {
+
+    var $table = 'sys_users';
+    var $column_order = ['sys_users.id', 'sys_users.uname', 'sys_users.role_id', 'sys_users.stat', 'sys_users.id']; //set column field database for datatable orderable
+    var $column_search = ['sys_users.uname', 'sys_roles.name']; //set column field database for datatable searchable 
+    var $order = ['sys_users.id' => 'asc']; // default order 
+
+    public function Signin($data) {
+        $exec = $this->db->query('CALL sys_auth("' . $data['uname'] . '");')->row();
+        mysqli_next_result($this->db->conn_id);
+        return $exec;
+    }
+
+    public function index($param) {
+        $exec = $this->db->query('CALL sys_users_select("' . $param['param'] . '",' . $param['id_user'] . ',' . $param['panjang'] . ',' . $param['mulai'] . ');');
+        mysqli_next_result($this->db->conn_id);
+        return $exec->result();
+    }
+
+    private function _filter() {
+        $role_id = Dekrip($this->session->userdata('role_id'));
+        $id_user = Dekrip($this->session->userdata('id_user'));
+        if ($role_id == sys_parameter('SUPER_USER')['param_value']) {
+            $exec = $this->db->select('sys_users.id,sys_users.uname,sys_users.pwd,sys_users.role_id,sys_users.pict,sys_users.stat,sys_roles.name');
+            $this->db->from($this->table)
+                    ->join('sys_roles', '`sys_users`.`role_id` = `sys_roles`.`id`');
+        } else {
+            $exec = $this->db->select('sys_users.id,sys_users.uname,sys_users.pwd,sys_users.role_id,sys_users.pict,sys_users.stat,sys_roles.name');
+            $this->db->from($this->table)
+                    ->join('sys_roles', '`sys_users`.`role_id` = `sys_roles`.`id`')
+                    ->where('`sys_users`.`id`', $id_user, false)
+                    ->or_where('`sys_roles`.`parent_id`', $role_id, false);
+        }
+        return $exec;
+    }
+
+    private function _get_datatables_query() {
+        $this->_filter();
+        $i = 0;
+        foreach ($this->column_search as $item) { // loop column 
+            if ($_GET['search']['value']) { // if datatable send POST for search
+                if ($i === 0) { // first loop
+                    $this->db->group_start(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
+                    $this->db->like($item, $_GET['search']['value']);
+                } else {
+                    $this->db->or_like($item, $_GET['search']['value']);
+                }
+
+                if (count($this->column_search) - 1 == $i) //last loop
+                    $this->db->group_end(); //close bracket
+            }
+            $i++;
+        }
+
+        if (isset($_GET['order'])) { // here order processing
+            $this->db->order_by($this->column_order[$_GET['order']['0']['column']], $_GET['order']['0']['dir']);
+        } else if (isset($this->order)) {
+            $order = $this->order;
+            $this->db->order_by(key($order), $order[key($order)]);
+        }
+    }
+
+    public function lists() {
+        $this->_get_datatables_query();
+        if ($_GET['length'] != -1)
+            $this->db->limit($_GET['length'], $_GET['start']);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function count_filtered() {
+        $this->_get_datatables_query();
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+
+    public function count_all() {
+        $this->_filter();
+        return $this->db->count_all_results();
+    }
+
+}
