bcc6349b17a9dbf845b0b469f5163545f4870505
maspriyambodo <maspriyambodo@gmail.com>
2022-03-02 15:54:47 +0700

    remove credential code

--- application/config.production/database.php
+++ application/config.production/database.php
@@ -3,11 +3,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = true;
-$db['default'] = ['dsn' => '',
-    'hostname' => 'localhost',
+$db['default'] = [
+    'dsn' => '',
+    'hostname' => 'your_host',
     'username' => 'your_username',
     'password' => 'your_password',
-    'database' => 'database_name',
+    'database' => 'your_database',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => false,
@@ -25,10 +26,32 @@ $db['default'] = ['dsn' => '',
 ];
 $db['simbi'] = [
     'dsn' => '',
-    'hostname' => '10.1.99.145',
-    'username' => 'root',
-    'password' => 'Masdatin2021!',
-    'database' => 'db_simbi',
+    'hostname' => 'your_host',
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'database' => 'your_database',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => false,
+    'db_debug' => false,
+    'cache_on' => false,
+    'cachedir' => '',
+    'char_set' => 'utf8mb4',
+    'dbcollat' => 'utf8mb4_general_ci',
+    'swap_pre' => '',
+    'encrypt' => false,
+    'compress' => false,
+    'stricton' => false,
+    'failover' => [],
+    'save_queries' => true
+];
+
+$db['epa'] = [
+    'dsn' => '',
+    'hostname' => 'your_host',
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'database' => 'your_database',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => false,
